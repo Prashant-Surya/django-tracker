9bc2509f1b4798c3e50b5bb5d2bad48150b3b035
Surya Prashanth <prashantsurya@ymail.com>
2016-09-18 00:52:57 +0530

    add fetch_visits view

--- middleware/views.py
+++ middleware/views.py
@@ -1,12 +1,17 @@
 from pymongo import MongoClient
+
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+
 client = MongoClient()
 db = client.tracker
 
-# Create your views here.
+# Create your views here
 
-
-def fetch_data(request):
-    data = db.tracker.aggregate(
+def fetch_visits(request):
+    template = 'visits.html'
+    data = db.visit_log.aggregate(
         [
             {
                 "$match":
@@ -24,5 +29,20 @@ def fetch_data(request):
             }
         ]
     )
+
+    graph_data = []
+    count = 1
     for document in data:
-        print(document)
+        #print(document)
+        graph_data.append({
+            'x': count,
+            'y': str(document['count'])
+        })
+        count += 1
+    ctx = {
+        'graph_data': graph_data
+    }
+
+    ctx = RequestContext(request, ctx)
+
+    return render_to_response(template, ctx)
