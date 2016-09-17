8221f60799bd4f02ea9b89047707c7001726c64a
richagupta-hackerearth <richa.gupta@hackerearth.com>
2016-09-17 20:10:55 +0530

    added function for fetching data

--- middleware/views.py
+++ middleware/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from pymongo import MongoClient
+client = MongoClient()
+db = client.tracker
 
 # Create your views here.
+
+
+def fetch_data(request):
+    data = db.tracker.aggregate(
+        [
+            {
+                "$match":
+                {
+                     "track_type": "Visit"
+                }
+            },
+            {
+                "$group": {
+                    "_id": "$url",
+                    "count": {
+                        "$sum": 1
+                    }
+                }
+            }
+        ]
+    )
+    for document in data:
+        print(document)
